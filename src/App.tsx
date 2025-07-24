d48d3cc366205a8470f25c23a6541238ab299fec
Z333Q <127899521+Z333Q@users.noreply.github.com>
2025-07-24 10:13:50 -0600

    Fix AgentSelector import to use named export

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ import Sidebar from './components/Sidebar';
 import RiskMonitor from './components/RiskMonitor';
 import TradeStream from './components/TradeStream';
 import BasketDetail from './components/BasketDetail';
-import AgentSelector from './components/AgentSelector';
+import { AgentSelector } from './components/AgentSelector';
 import ConnectWizard from './components/ConnectWizard';
 import ProofExplorer from './components/ProofExplorer';
 import WalletStaking from './components/WalletStaking';
