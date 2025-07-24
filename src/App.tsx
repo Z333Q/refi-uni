2617bcd328395f13a1d997e6d75859d731dd346c
Z333Q <127899521+Z333Q@users.noreply.github.com>
2025-07-24 17:21:02 -0600

    Align logo and wordmark to the left

--- src/App.tsx
+++ src/App.tsx
@@ -359,7 +359,7 @@ function App() {
             <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20 pb-16">
               <div className="text-center">
                 {/* Logo */}
-                <div className="flex justify-center items-center space-x-4 mb-8">
+                <div className="flex justify-start items-center space-x-4 mb-8">
                   <img 
                     src="/green-logo-only-squareArtboard 1@0.25x.png" 
                     alt="ReFi.Trading Logo" 
